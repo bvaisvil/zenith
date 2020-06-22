d7cce6adef12232b62bf360d3b61a196156c8d89
Benjamin Vaisvil <ben@neuon.com>
2020-06-21 22:59:07 -0500

    refactor to use traits

--- src/graphics_nvidia.rs
+++ src/graphics_nvidia.rs
@@ -3,30 +3,81 @@
  */
 use crate::graphics::*;
 use crate::metrics::CPUTimeApp;
+use nvml::device::Device;
 use nvml::enum_wrappers::device::{Clock, TemperatureSensor, TemperatureThreshold};
 use nvml::struct_wrappers::device::ProcessUtilizationSample;
 use nvml::NVML;
+use std::convert::TryFrom;
 use std::fmt;
 
-impl GraphicsDeviceProcess {
-    fn from_nvml(process: &ProcessUtilizationSample) -> GraphicsDeviceProcess {
+impl From<&ProcessUtilizationSample> for GraphicsDeviceProcess {
+    fn from(sample: &ProcessUtilizationSample) -> GraphicsDeviceProcess {
         GraphicsDeviceProcess {
-            pid: process.pid as i32,
-            timestamp: process.timestamp,
-            sm_utilization: process.sm_util,
-            mem_utilization: process.mem_util,
-            enc_utilization: process.enc_util,
-            dec_utilization: process.dec_util,
+            pid: sample.pid as i32,
+            timestamp: sample.timestamp,
+            sm_utilization: sample.sm_util,
+            mem_utilization: sample.mem_util,
+            enc_utilization: sample.enc_util,
+            dec_utilization: sample.dec_util,
         }
     }
 }
 
-impl GraphicsDevice {
-    pub(crate) fn processes_from_nvml(&mut self, processes: Vec<ProcessUtilizationSample>) {
-        self.processes = processes
-            .iter()
-            .map(|p| GraphicsDeviceProcess::from_nvml(p))
-            .collect()
+impl TryFrom<&Device<'_>> for GraphicsDevice {
+    type Error = nvml::error::Error;
+
+    fn try_from(d: &Device<'_>) -> Result<Self, Self::Error> {
+        let uuid = d.uuid()?;
+        let mut gd = GraphicsDevice::new(uuid);
+        match d.memory_info() {
+            Ok(m) => {
+                gd.total_memory = m.total;
+                gd.used_memory = m.used;
+            }
+            Err(e) => error!("Failed Getting Memory Info {:?}", e),
+        }
+        gd.name = d.name().unwrap_or(String::from(""));
+        gd.clock = d.clock_info(Clock::Graphics).unwrap_or(0);
+        gd.max_clock = d.max_clock_info(Clock::Graphics).unwrap_or(0);
+        gd.power_usage = d.power_usage().unwrap_or(0);
+        gd.max_power = d.power_management_limit().unwrap_or(0);
+        gd.temperature = d.temperature(TemperatureSensor::Gpu).unwrap_or(0);
+        gd.temperature_max = d
+            .temperature_threshold(TemperatureThreshold::GpuMax)
+            .unwrap_or(0);
+        for i in 0..4 {
+            // get the speed of up to 5 fans;
+            let r = d.fan_speed(i);
+            if r.is_ok() {
+                gd.fans.push(r.unwrap_or(0));
+            } else {
+                break;
+            }
+        }
+        gd.decoder_utilization = match d.decoder_utilization() {
+            Ok(u) => u.utilization,
+            Err(_) => 0,
+        };
+        gd.encoder_utilization = match d.encoder_utilization() {
+            Ok(u) => u.utilization,
+            Err(_) => 0,
+        };
+        match d.utilization_rates() {
+            Ok(u) => {
+                gd.gpu_utilization = u.gpu;
+                gd.mem_utilization = u.memory;
+            }
+            Err(e) => error!("Couldn't get utilization rates: {:?}", e),
+        }
+        match d.process_utilization_stats(None) {
+            Ok(ps) => gd.processes = ps.iter().map(|p| GraphicsDeviceProcess::from(p)).collect(),
+            Err(_) => debug!(
+                "Couldn't retrieve process utilization stats for {:}",
+                gd.name
+            ),
+        }
+
+        Ok(gd)
     }
 }
 
@@ -61,65 +112,22 @@ impl GraphicsExt for CPUTimeApp {
                     continue;
                 }
             };
-
-            let uuid = match d.uuid() {
-                Ok(uuid) => uuid,
+            let gd = match GraphicsDevice::try_from(&d) {
+                Ok(gd) => gd,
                 Err(e) => {
-                    error!("Couldn't get UUID: {}", e);
+                    error!("Failed Getting Memory Info {:?}", e);
                     continue;
                 }
             };
-            let mut gd = GraphicsDevice::new(uuid);
-            match d.memory_info() {
-                Ok(m) => {
-                    gd.total_memory = m.total;
-                    gd.used_memory = m.used;
-                }
-                Err(e) => error!("Failed Getting Memory Info {:?}", e),
-            }
-            gd.name = d.name().unwrap_or(String::from(""));
-            gd.clock = d.clock_info(Clock::Graphics).unwrap_or(0);
-            gd.max_clock = d.max_clock_info(Clock::Graphics).unwrap_or(0);
-            gd.power_usage = d.power_usage().unwrap_or(0);
-            gd.max_power = d.power_management_limit().unwrap_or(0);
-            gd.temperature = d.temperature(TemperatureSensor::Gpu).unwrap_or(0);
-            gd.temperature_max = d
-                .temperature_threshold(TemperatureThreshold::GpuMax)
-                .unwrap_or(0);
-            for i in 0..4 {
-                let r = d.fan_speed(i);
-                if r.is_ok() {
-                    gd.fans.push(r.unwrap_or(0));
-                } else {
-                    break;
-                }
-            }
-            gd.decoder_utilization = match d.decoder_utilization() {
-                Ok(u) => u.utilization,
-                Err(_) => 0,
-            };
-            gd.encoder_utilization = match d.encoder_utilization() {
-                Ok(u) => u.utilization,
-                Err(_) => 0,
-            };
-            match d.utilization_rates() {
-                Ok(u) => {
-                    self.histogram_map
-                        .add_value_to(format!("{}_gpu", gd.uuid).as_str(), u.gpu as u64);
-                    self.histogram_map
-                        .add_value_to(format!("{}_mem", gd.uuid).as_str(), u.memory as u64);
-                    gd.gpu_utilization = u.gpu;
-                    gd.mem_utilization = u.memory;
-                }
-                Err(e) => error!("Couldn't get utilization rates: {:?}", e),
-            }
-            match d.process_utilization_stats(None) {
-                Ok(ps) => gd.processes_from_nvml(ps),
-                Err(_) => debug!(
-                    "Couldn't retrieve process utilization stats for {:}",
-                    gd.name
-                ),
-            }
+            self.histogram_map.add_value_to(
+                format!("{}_gpu", gd.uuid).as_str(),
+                gd.gpu_utilization as u64,
+            );
+            self.histogram_map.add_value_to(
+                format!("{}_mem", gd.uuid).as_str(),
+                gd.mem_utilization as u64,
+            );
+
             debug!("{:}", gd);
             // mock device code to test multiple cards.
             //let mut gd2 = gd.clone();
