5fcce244c98fbb1707cbc03128162966bb1b71e7
Sumedh Wale <sumwale@gmail.com>
2020-10-17 05:43:23 +0530

    change NVIDIA check from nvidia-uvm to nvidiactl

--- assets/zenith.sh
+++ assets/zenith.sh
@@ -2,7 +2,7 @@
 
 PREFIX=/usr/local
 
-if test -r /dev/nvidia-uvm && "$PREFIX/lib/zenith/zenith-libnvidia-detect"; then
+if test -r /dev/nvidiactl && "$PREFIX/lib/zenith/zenith-libnvidia-detect"; then
   exec "$PREFIX/lib/zenith/nvidia/zenith" "$@"
 else
   exec "$PREFIX/lib/zenith/base/zenith" "$@"
