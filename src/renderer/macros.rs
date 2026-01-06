8d33ecb6b52cc79011bb2b08fcaee2ddcbe902e0
Benjamin Vaisvil <benjamini@gmail.com>
2026-01-06 12:37:29 -0600

    cargo fmt!

--- src/renderer/macros.rs
+++ src/renderer/macros.rs
@@ -5,11 +5,9 @@
 macro_rules! float_to_byte_string {
     ($x:expr, $unit:expr) => {
         match Byte::from_f64_with_unit($x, $unit) {
-            Some(b) => format!(
-                "{:.2}",
-                b.get_appropriate_unit(byte_unit::UnitType::Binary)
-            )
-            .replace(" ", "").replace("iB", ""),
+            Some(b) => format!("{:.2}", b.get_appropriate_unit(byte_unit::UnitType::Binary))
+                .replace(" ", "")
+                .replace("iB", ""),
             None => String::from("Err"),
         }
     };
